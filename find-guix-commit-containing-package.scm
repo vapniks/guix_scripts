abd516c4beb560b38be648e88657a36663bf7e2b
Joe Bloggs <vapniks@yahoo.com>
2025-01-14 13:28:03 +0000

    Fix manifest-to-manifest.scm, and add find-guix-commit-containing-package.scm

---
+++ find-guix-commit-containing-package.scm
@@ -0,0 +1,50 @@
+#!/home/ben/.guix-profile/bin/guile -s
+!#
+
+(use-modules (guix profiles) (guix store) (srfi srfi-1) (guix build utils))
+
+(define (find-guix-commit package-name package-version)
+  ;; Check manifests in all generations of the current profile
+  (let ((pkgentry #f)
+	(guixentry #f)
+	(pull-profile "/var/guix/profiles/per-user/root/current-guix")
+	(pull-generation #f)
+	(chash #f))
+    ;; loop over all generations of the user profile
+    (do ((ns (profile-generations %current-profile) (cdr ns)))
+	((or pkgentry (null-list? ns)))
+      ;; get the manifest entry for the package@version we're looking for if it exists in this profile generation
+      (set! pkgentry (manifest-lookup (profile-manifest (generation-file-name %current-profile (car ns)))
+				      (manifest-pattern (name package-name)
+							(version package-version)))))
+    (when pkgentry
+      ;; get the commit hash of the version of guix that produced the package
+      (set! chash (substring
+		   (cadr (assq 'commit
+			       (cdadr (assq 'provenance
+					    (manifest-entry-properties pkgentry)))))
+		   0 7))
+      ;; loop over the pull profile generations
+      (do ((ns (profile-generations pull-profile) (cdr ns)))
+	  (guixentry)
+	(set! pull-generation (car ns))
+	;; get the manifest entry for guix@chash if it exists in this profile generation
+	(set! guixentry (manifest-lookup (profile-manifest (generation-file-name pull-profile pull-generation))
+					 (manifest-pattern (name "guix")
+							   (version chash))))))
+    ;; return the pull generation and commit hash
+    (values pull-generation chash)))
+
+;; Run this when called from the command line:
+(let ((cmdline (command-line)))
+  (when (> (length cmdline) 1)
+    (if (< (length cmdline) 3)
+	(format #t "Usage: ~a <PKG> <VERSION>~%" (basename (current-filename)))
+	(let ((pkg (cadr cmdline))
+	      (version (caddr cmdline)))
+	  (call-with-values (lambda () (find-guix-commit pkg version))
+	    (lambda (pullgen chash)
+	      (if pullgen
+		  (format #t "Package ~a@~a found in pull generation: ~a, with commit hash: ~a~%"
+			  pkg version pullgen chash)
+		  (format #t "Can't find guix pull profile for version ~a of ~a~%" version pkg))))))))
--- manifest-to-manifest.scm
+++ manifest-to-manifest.scm
@@ -1,4 +1,5 @@
-#!guile -s
+#!guile \
+-e main -s
 !#
 ;; For usage instructions do: manifest-to-manifest.scm -h
 
@@ -6,6 +7,7 @@
 ;; Some changes made by me.
 
 (use-modules (guix profiles)
+	     (gnu packages)
              (ice-9 match)
              (ice-9 pretty-print)
 	     (ice-9 getopt-long))
@@ -26,37 +28,35 @@
         string<?))
 
 (define (guix-commit)
-  (let ((guix-manifest (profile-manifest (string-append (getenv "HOME") 
-							"/.config/guix/current"))))
-    (match (assq 'source (manifest-entry-properties (car (manifest-entries 
-							  guix-manifest))))
-      (('source ('repository ('version 0) _ _
-                             ('commit commit) _ ...))
-       commit)
-      (_ #f))))
-
-(let* ((cl (command-line))
-       (optspec `((help (single-char #\h)
-			(required? #f))
-		  (profile (single-char #\p)
-			   (required? #f)
-			   (value #t)
-			   (predicate ,(lambda (p)
-					 (file-exists?
-					  (string-append p "/manifest")))))
-		  (include-versions (single-char #\v)
-				    (required? #f)
-				    (value #f))
-		  (output (single-char #\o)
-			  (required? #f)
-			  (value #t))))
-       (opts (getopt-long (command-line) optspec))
-       (profile (option-ref opts 'profile (getenv "GUIX_PROFILE")))
-       (incv (option-ref opts 'include-versions #f))
-       (help (option-ref opts 'help #f))
-       (outfile (option-ref opts 'output #f)))
-  (if help
-      (format #t "Usage: manifest-to-manifest.scm [OPTION]...
+  (substring
+   (cadr (assq 'commit
+	       (cdadr (assq 'provenance
+			    (manifest-entry-properties (car (manifest-entries 
+							     (profile-manifest %current-profile))))))))
+   0 7))
+  
+(define (main args) 
+  (let* ((optspec `((help (single-char #\h)
+			  (required? #f))
+		    (profile (single-char #\p)
+			     (required? #f)
+			     (value #t)
+			     (predicate ,(lambda (p)
+					   (file-exists?
+					    (string-append p "/manifest")))))
+		    (include-versions (single-char #\v)
+				      (required? #f)
+				      (value #f))
+		    (output (single-char #\o)
+			    (required? #f)
+			    (value #t))))
+	 (opts (getopt-long args optspec))
+	 (profile (option-ref opts 'profile %current-profile))
+	 (incv (option-ref opts 'include-versions #f))
+	 (help (option-ref opts 'help #f))
+	 (outfile (option-ref opts 'output #f)))
+    (if help
+	(format #t "Usage: ~a [OPTION]...
 Create manifest object code for packages in a guix profile.
 The output can be used with the -m option of \"guix profile\"
 
@@ -65,11 +65,14 @@ Options:
  -p, --profile=SYMLINK   Specify guix profile (default=~a)
  -v, --include-versions  Include package versions in output
  -o, --output=FILE       Write output to FILE instead of stdout
-" (getenv "GUIX_PROFILE"))
-      (let ((out (if outfile
-		     (open-output-file outfile)
-		     (current-output-port))))
-	(format out ";; Using guix commit: ~a\n" (guix-commit))
-	(format out ";; Manifest generated from ~a\n" profile)
-	(pretty-print `(specifications->manifest ',(guix-manifest profile incv)) out))))
+"
+		(basename (current-filename))
+		%current-profile)
+	(let ((out (if outfile
+		       (open-output-file outfile)
+		       (current-output-port))))
+	  (format out ";; Using guix commit: ~a\n" (guix-commit))
+	  (format out ";; Manifest generated from ~a\n" profile)
+	  (pretty-print `(specifications->manifest ',(guix-manifest profile incv)) out)
+	  ))))
 
